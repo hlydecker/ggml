947dcd8cbb0862f7b62d67c867cd15c4e119c659
Henry Lydecker <henry.lydecker@gmail.com>
2022-03-29 14:05:17 +1100

    Create gg_varcorr.R

---
+++ R/gg_varcorr.R
@@ -0,0 +1,22 @@
+# TODO: make actually good
+
+library(corrr)
+vars_to_exclude <- c("treatment","data_group","rhythm_during_echo")
+
+gg_variable_correlation <- function(dataframe, target_var){
+  
+  df_cor <- dataframe %>%
+    correlate() %>%
+    corrr::focus(target_var)
+  
+  df_cor %>%
+    mutate(term = fct_reorder(term, target_var)) %>%
+    ggplot(aes(x = target_var, y = term, fill = abs(target_var))) +
+    geom_bar(stat = "identity") +
+    geom_vline(xintercept = 0.5, color = "black") +
+    geom_vline(xintercept = -0.5, color = "black") +
+    scale_fill_gradientn(colours = usyd_palette("flametree", 100, type = "continuous")) +
+    labs(y = NULL,
+         x = "Age") +
+    theme(legend.position = "none")
+}
